0242adfdbe4d76919985144e3b67da151b16c191
Ana <davybead@gmail.com>
2021-07-15 21:47:28 +0500

    1st lesson

---
+++ 2dGame/Assets/Scripts/Main.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace PlatformerMVC
+{
+  public class Main : MonoBehaviour
+  {
+      [SerializeField] private SpriteAnimatorController _playerAnimator;
+      [SerializeField] private LevelObjectView _playerView;
+      [SerializeField] private AnimationCfg _playerConfig;
+      [SerializeField] private int _animationSpeed = 10;
+      
+      void Awake()
+      {
+          _playerConfig = Resources.Load<AnimationCfg>("PlayerAnimCtg");
+          _playerAnimator = new SpriteAnimatorController(_playerConfig);
+          _playerAnimator.StartAnimation(_playerView._spriteRenderer, AnimState.Run, true, _animationSpeed);
+      }
+      
+      void Update()
+      {
+          _playerAnimator.Update();
+      }
+  }  
+}
+
---
+++ 2dGame/Assets/Scripts/View/LevelObjectView.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlatformerMVC
+{
+    public class LevelObjectView : MonoBehaviour
+    {
+        public Transform _transform;
+        public SpriteRenderer _spriteRenderer;
+
+        public SpriteRenderer SpriteRenderer { get; set; }
+    }
+}
